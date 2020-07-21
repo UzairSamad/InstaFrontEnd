968ad67f131d5c17afd307934ccb92f639c0c4dd
M.Uzair <uzair.iu17@gmail.com>
2020-07-21 18:34:44 +0500

    created login forrm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './App.css'
+
 import { makeStyles } from '@material-ui/core/styles';
 import NavBar from './components/Navbar'
 import Home from './screens/Home'
 import Profile from './screens/Profile'
 import Login from './screens/Login'
 import SignUp from './screens/SignUp'
+// import './App.css'
 
 import { BrowserRouter, Route } from 'react-router-dom'
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
-const Login = ()=>{
+const Login = () => {
 
-return(
-    <h1>Login</h1>
-)
+    return (
+        <div  class="mycard">
+            <div class="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
+                <button class="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                    Login
+                </button>
+
+            </div>
+        </div>
+    )
 
 }
 
