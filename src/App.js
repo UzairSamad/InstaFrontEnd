15809cc7162bcde2a4ba9902feed3dd7d7a775c7
M.Uzair <uzair.iu17@gmail.com>
2020-07-21 17:16:19 +0500

    created initiAL SCREEN AND ROUTES WITH REACT ROUTER DOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import NavBar from './components/Navbar'
+import Home from './screens/Home'
+import Profile from './screens/Profile'
+import Login from './screens/Login'
+import SignUp from './screens/SignUp'
+
+import { BrowserRouter, Route } from 'react-router-dom'
 
 const App = () => (
+  <BrowserRouter >
+    <NavBar />
+    <Route path="/" exact>
+      <Home />
+    </Route >
 
-  <div>
-    My name is uzair
-  </div>
+    <Route path="/profile">
+      <Profile />
+    </Route >
+
+    <Route path="/login">
+      <Login />
+    </Route >
+
+    <Route path="/signup">
+      <SignUp />
+    </Route >
+
+  </BrowserRouter>
+
 )
 export default App
