26356bcddf9bfc1c6701f5a7622dc7ee74811033
M.Uzair <uzair.iu17@gmail.com>
2020-07-28 13:01:27 +0500

    dynamic my posts

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Profile = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        let token = localStorage.getItem("token")
+        fetch('/my-posts', {
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        }).then(res => res.json()).then(result => {
+            console.log(result)
+            setData(result.posts)
+            console.log(data, 'data')
+        }).catch(err => console.log(err))
+    }, [])
 
     return (
-        <div style={{maxWidth:'550px',margin:'0px auto'}}>
+        <div style={{ maxWidth: '550px', margin: '0px auto' }}>
             <div style={{ display: 'flex', justifyContent: 'space-around', margin: '18px 0px', borderBottom: '1px solid grey' }}>
                 <div>
                     <img style={{ width: '160px', height: '160px', borderRadius: '80px' }}
@@ -12,21 +25,18 @@ const Profile = () => {
                 </div>
                 <div>
                     <h4>Ladan JAfrii</h4>
-                    <div style={{ display: "flex",justifyContent:"space-between",width:'108%' }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", width: '108%' }}>
                         <h5>40 Posts</h5>
                         <h5>41 Followers</h5>
                         <h5>20 Following</h5>
                     </div>
                 </div>
             </div>
-        <div style={{display:'flex',justifyContent: 'space-between',flexWrap:'wrap'}}>
-        <img className='gallery-item' src="https://i.ytimg.com/vi/zAGVQLHvwOY/hqdefault.jpg"/>
-        <img className='gallery-item' src="https://i.ytimg.com/vi/zAGVQLHvwOY/hqdefault.jpg"/>
-        <img className='gallery-item' src="https://i.ytimg.com/vi/zAGVQLHvwOY/hqdefault.jpg"/>
-        <img className='gallery-item' src="https://i.ytimg.com/vi/zAGVQLHvwOY/hqdefault.jpg"/>
-        <img className='gallery-item' src="https://i.ytimg.com/vi/zAGVQLHvwOY/hqdefault.jpg"/>
-        <img className='gallery-item' src="https://i.ytimg.com/vi/zAGVQLHvwOY/hqdefault.jpg"/>
-        </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+                {data.map(item => {
+                    return <img style={{ width:'30%',marginBottom:'10px',height:'200px'}} src={item.photo} />
+                })}
+            </div>
 
         </div>
     )
