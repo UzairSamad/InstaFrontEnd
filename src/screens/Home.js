fa58a13926939e8c7d3115f4dcd59d60f0a4a619
M.Uzair <uzair.iu17@gmail.com>
2020-07-29 11:13:08 +0500

    done with comments

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,7 +4,7 @@ import { UserContext } from '../App'
 const Home = () => {
     const [data, setData] = useState([])
     const { state, dispatch } = useContext(UserContext)
-    console.log(state, 'stateee')
+    console.log(data, ' ')
 
     useEffect(() => {
         let token = localStorage.getItem("token")
@@ -62,6 +62,31 @@ const Home = () => {
             setData(newData)
         }).catch(err => console.log(err))
     }
+    const addComment = (text, postId) => {
+        let token = localStorage.getItem("token")
+
+        fetch('/comment', {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token
+            },
+            body: JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res => res.json()).then(result => {
+            const newData = data.map(item => {
+                if (item._id === result._id) {
+                    return result
+                } else {
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err => console.log(err))
+
+    }
 
     return (
         <div className='home'>
@@ -84,7 +109,17 @@ const Home = () => {
 
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
-                                <input type='text' placeholder='Add a comment' />
+                                {
+                                    item.comments.map(record => {
+                                        return <h6 key={record._id} ><span style={{ fontWeight: "500"}}>{record.postedBy.name}</span>{record.text }</h6>
+                                    })
+                                }
+                                <form onSubmit={(e) => {
+                                    e.preventDefault()
+                                    addComment(e.target[0].value,item._id)
+                                }}>
+                                    <input type='text' placeholder='Add a comment' />
+                                </form>
                             </div>
                         </div>
                     )
